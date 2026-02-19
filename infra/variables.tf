320fc8b43ee27bfb6d5e8471900f593f489125c5
Filipe Almeida <filipe_guimaraes@outlook.com>
2026-02-18 23:49:39 -0300

    Alterando .gitignore, variables etc

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,9 @@
-variable "vpc_cidr_block"{
-  type = string
+variable "vpc_cidr_block" {
+  type    = string
   default = "172.16.0.0/16"
 }
 
 variable "vpc_name" {
-  type = string
+  type    = string
   default = "environment-vpc"
 }
